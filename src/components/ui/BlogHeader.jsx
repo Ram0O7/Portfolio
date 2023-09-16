0a4390a10e0ecac8c721a5ee5b9e5d78ac38261b
Ramkrishn <ram706860@gmail.com>
2023-09-16 13:39:50 +0530

    add colored tags

--- src/components/ui/BlogHeader.jsx
+++ src/components/ui/BlogHeader.jsx
@@ -1,4 +1,17 @@
 const BlogHeader = ({ title, tags }) => {
+  const tagColors = {
+    javascript: "text-yellow-500",
+    html: "text-red-500",
+    css: "text-blue-500",
+    accessibility: "text-gray-500",
+    website: "text-green-500",
+    app: "text-cyan-500",
+    programming: "text-pink-500",
+    career: "text-violet-500",
+    development: "text-rose-500",
+  };
+  const keys = Object.keys(tagColors);
+
   return (
     <>
       <h1>{title}</h1>
@@ -6,7 +19,14 @@ const BlogHeader = ({ title, tags }) => {
         {tags.map((tag) => {
           return (
             <p key={tag}>
-              <span>#</span>
+              <span
+                className={
+                  tagColors[tag.toLowerCase()] ||
+                  tagColors[keys[Math.floor(Math.random() * keys.length)]]
+                }
+              >
+                #
+              </span>
               {tag}
             </p>
           );
